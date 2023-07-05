2b377041c14c81d8d766074151485d7e1c4ffbf3
Vladyslav <ipz201_svo@student.ztu.edu.ua>
2023-07-05 22:46:39 +0300

    add logging

--- TestDeployment/Program.cs
+++ TestDeployment/Program.cs
@@ -1,9 +1,17 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    .WriteTo.Console());
 
 var app = builder.Build();
 
@@ -12,7 +20,11 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/guid", () => Guid.NewGuid())
+app.MapGet("/api/guid", (ILogger<Program> logger) => { 
+    var guid = Guid.NewGuid();
+    logger.LogWarning("GUID {Guid} was created.", guid.ToString());
+    return guid;
+})
     .WithName("GetGuid")
     .WithOpenApi();
 
